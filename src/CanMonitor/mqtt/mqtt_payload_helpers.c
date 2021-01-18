d65d00a5eae8490245bee2e8427bd170105d5568
miglesias <miglesias@ikerlan.es>
2021-01-18 15:48:10 +0100

    Updated to use kebab-case variables on mqtt payloads.

--- src/CanMonitor/mqtt/mqtt_payload_helpers.c
+++ src/CanMonitor/mqtt/mqtt_payload_helpers.c
@@ -30,7 +30,7 @@ const char *create_discovery_payload(void)
 
     json_object_set_string(robj, "id", monitor_id);
     json_object_set_string(robj, "name", monitor_id);
-    json_object_set_string(robj, "microserviceType", "monitor-agent");
+    json_object_set_string(robj, "microservice-type", "monitor-agent");
     
     JSON_Value *branch = json_value_init_array();
     JSON_Array *leaves = json_value_get_array(branch);
@@ -38,17 +38,17 @@ const char *create_discovery_payload(void)
     JSON_Value *leaf_value_1 = json_value_init_object();
     JSON_Object *leaf_object_1 = json_value_get_object(leaf_value_1);
     json_object_set_string(leaf_object_1, "endpoint-type", "mqtt");
-    json_object_set_string(leaf_object_1, "iP", mqtt_broker_host);
+    json_object_set_string(leaf_object_1, "ip", mqtt_broker_host);
     json_object_set_number(leaf_object_1, "port", mqtt_broker_port);
-    json_object_set_number(leaf_object_1, "qoS", mqtt_qos);
+    json_object_set_number(leaf_object_1, "qos", mqtt_qos);
     json_array_append_value(leaves, leaf_value_1);
 
     JSON_Value *leaf_value_2 = json_value_init_object();
     JSON_Object *leaf_object_2 = json_value_get_object(leaf_value_2);
     json_object_set_string(leaf_object_2, "endpoint-type", "http");
-    json_object_set_string(leaf_object_2, "iP", "");
+    json_object_set_string(leaf_object_2, "ip", "");
     json_object_set_number(leaf_object_2, "port", rest_server_port);
-    json_object_set_number(leaf_object_2, "qoS", 0);
+    json_object_set_number(leaf_object_2, "qos", 0);
     json_array_append_value(leaves, leaf_value_2);
 
     json_object_set_value(robj, "endpoints", branch);
